0aa8b0ea9a3c430da095143f87c2086eb4e56b78
Mukhamedjan <muklia@MacBook-Pro-Mukhamedjan.local>
2018-06-28 18:03:21 +0600

    First commit

---
+++ FlappyBird/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  FlappyBird
+//
+//  Created by Mukhamedjan on 01.05.17.
+//  Copyright © 2017 sdu. All rights reserved.
+//
+import Foundation
+
+class LocalizedStrings {
+    
+}
